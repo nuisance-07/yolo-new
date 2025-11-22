3b05c39762e47561ee86c37587d5ff35bd79898f
Mwangi121 <collins.mwangi4@student.moringaschool.com>
2025-11-22 13:45:52 +0300

    refactor: encapsulate product schema definition within a Schema object

--- backend/models/Products.js
+++ backend/models/Products.js
@@ -1,35 +1,36 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
-name: String,
+let productSchema = new Schema({
+    name: String,
     description: String,
-        price: Number,
-            quantity: Number,
-                photo: String,
-                    category: String,
-                        reviews: [
-                            {
-                                user: {
-                                    type: Schema.Types.ObjectId,
-                                    ref: 'user'
-                                },
-                                name: {
-                                    type: String
-                                },
-                                rating: {
-                                    type: Number,
-                                    required: true
-                                },
-                                comment: {
-                                    type: String,
-                                    required: true
-                                },
-                                date: {
-                                    type: Date,
-                                    default: Date.now
-                                }
-                            }
-                        ]
+    price: Number,
+    quantity: Number,
+    photo: String,
+    category: String,
+    reviews: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'user'
+            },
+            name: {
+                type: String
+            },
+            rating: {
+                type: Number,
+                required: true
+            },
+            comment: {
+                type: String,
+                required: true
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ]
 });
 
 let Product = mongoose.model('Product', productSchema)
